8f8465664d8d3d811ed24eed9c091c5153f9b8ea
Ramon-Goveia <ramonalangoveia@hotmail.com>
2021-06-27 15:13:05 -0300

    Class Members

--- src/main/kotlin/one/digitalinnovation/diobank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/diobank/Pessoa.kt
@@ -3,13 +3,14 @@ package one.digitalinnovation.diobank
 class Pessoa {
     var nome: String = "Ramon"
     var cpf: String = "111.111.111.11"
-
     private set
+
+    constructor()
+
+    fun pessoaInfo() = "$nome e $cpf"
 }
 
 fun main() {
     val kotlin = Pessoa()
-
-    println(kotlin.nome)
-    println(kotlin.cpf)
+    println(kotlin.pessoaInfo())
 }
