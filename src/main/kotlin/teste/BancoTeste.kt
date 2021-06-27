b376161d0f44244915f23532ddbfc7fa95cf5a25
Ramon-Goveia <ramonalangoveia@hotmail.com>
2021-06-27 15:51:41 -0300

    Bank Data Class

---
+++ src/main/kotlin/teste/BancoTeste.kt
@@ -0,0 +1,14 @@
+package teste
+
+import one.digitalinnovation.diobank.Banco
+
+fun main(){
+    val dioBank = Banco("DioBank", 33 )
+
+    println(dioBank.nome)
+    println(dioBank.numero)
+
+    val banco2 = dioBank.copy(nome = "Nome2")
+
+    println(banco2.info())
+}
