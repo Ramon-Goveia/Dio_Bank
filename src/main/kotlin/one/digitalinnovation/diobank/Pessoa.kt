c4e66518aaed7e7ae39045e726add3f6fe7e6f03
Ramon-Goveia <ramonalangoveia@hotmail.com>
2021-06-27 14:55:43 -0300

    Inner Class

--- src/main/kotlin/one/digitalinnovation/diobank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/diobank/Pessoa.kt
@@ -3,6 +3,10 @@ package one.digitalinnovation.diobank
 class Pessoa {
     var nome: String = "Ramon"
     var cpf: String = "111.111.111.11"
+
+    inner class Endereco{
+        var rua: String = "Rua Teste"
+    }
 }
 
 fun main() {
@@ -10,4 +14,5 @@ fun main() {
 
     println(kotlin.nome)
     println(kotlin.cpf)
+    println(kotlin.Endereco().rua)
 }
